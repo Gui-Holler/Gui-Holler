335a300b27c472f489de40761a0e168ffc9f6b7a
Gui-Holler <91102689+Gui-Holler@users.noreply.github.com>
2022-01-17 15:26:40 -0300

    Add files via upload

---
+++ map.js
@@ -0,0 +1,32 @@
+const orange = {
+	price: 2,
+};
+
+const apple = {
+	price: 1.5,
+};
+
+function mapArray() {
+	const array = [1, 2, 3, 4, 5];
+
+	return array.map(function (item) {
+		return item * this.price;
+	}, apple);
+}
+
+console.log(mapArray());
+
+
+/* map sem this
+
+function mapSemThis (arr) {
+    return arr.map(function(item){
+        return item * 2;
+    });
+}
+
+const nums = [2, 4, 6, 8, 10];
+
+console.log(mapSemThis(nums));
+
+console.log(nums);*/
